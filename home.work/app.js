cb511c80e9e12fd35f4e29f52c0d8168e1bfb543
Jakyp <jakypjumagulov080@gmail.com>
2023-05-24 15:32:08 +0600

    home work

---
+++ home.work/app.js
@@ -0,0 +1,10 @@
+var gmail = prompt("gmail жаз")
+
+var password = prompt("password жаз")
+if(gmail === 'jakyp@gmail.com'){
+    console.log('Yes');
+}else if(password === 'jakyp-05'){
+    console.log('Yes');
+}else{
+    console.log('---');
+}
